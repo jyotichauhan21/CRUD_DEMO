dd66d5d5208ba2e03f1dcedf033254e5b4c106f9
jyoti <jyoti@DESKTOP-PUS43LR>
2022-11-07 12:26:55 +0530

    Add project files.

---
+++ CRUD_DEMO/Data/MVCDemoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CRUD_DEMO.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace CRUD_DEMO.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ CRUD_DEMO/Models/Domain/Product.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUD_DEMO.Models.Domain
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+    }
+}
